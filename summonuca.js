b44b274bdca59b656c430c161e8c5cb422d04ec9
mborg <mattborg1@gmail.com>
2018-01-04 12:48:23 +0000

    Add files via upload

--- summonuca.js
+++ summonuca.js
@@ -8,7 +8,7 @@ function() {
             if (showAlert)
                 iElement
                     .find('ul.list-unstyled')
-                    .prepend('<div id="ucaemail" style="color: #a94442;background-color: #f2dede;border-color: #ebccd1;padding: 15px;margin-bottom: 20px;border: 1px solid transparent;border-radius: 4px;">Need help? <a mailto=@help@university.ac.uk">Email us!</a></div>');
+                    .prepend('<div id="ucaemail" style="color: #a94442;background-color: #f2dede;border-color: #ebccd1;padding: 15px;margin-bottom: 20px;border: 1px solid transparent;border-radius: 4px;">Need help? <a mailto=help@university.ac.uk">Email us!</a></div>');
                 
         }
     }

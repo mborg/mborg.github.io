e45cdfb9dbfb93dddf5260416582ed261a555a18
mborg <mattborg1@gmail.com>
2018-03-01 13:26:12 +0000

    Update summon.js

--- summon.js
+++ summon.js
@@ -13,3 +13,8 @@ function() {
         }
     }
 });
+
+/// Make the logo bigger (from Matt Reidsma)
+
+    $(".siteHeader img.img-responsive.logo").attr("src", "https://www.gvsu.edu/includes/topbanner/3/gvsu_logo.png");
+      console.log('Made the logo bigger.');

4ecb9ba86d43052f8c709a179fc40aa2597df948
mborg <mattborg1@gmail.com>
2018-03-09 08:45:46 +0000

    Update summon.js

--- summon.js
+++ summon.js
@@ -27,7 +27,7 @@ function() {
             if (showAlert)
                 iElement
                     .find('ul.list-unstyled')
-                    .prepend('<div id="uwlfeedback" style="color: #a94442;background-color: #f2dede;border-color: #ebccd1;padding: 15px;margin-bottom: 20px;border: 1px solid transparent;border-radius: 4px;">Need help referencing? Log in to <a href="https://shibboleth2sp.pm.semcs.net/Shibboleth.sso/Login?entityID=https%3A%2F%2Fidp.uwl.ac.uk%2Fidp%2Fshibboleth&target=https%3A%2F%2Fshibboleth2sp.pm.semcs.net%2Fshib%3Fdest%3Dhttp%253A%252F%252Fwww.citethemrightonline.com%252FShibValidate">Cite Them Right</a></div>');
+                    .prepend('<div id="uwlfeedback" style="color: #a94442;background-color: #f2dede;border-color: #ebccd1;padding: 15px;margin-bottom: 20px;border: 1px solid transparent;border-radius: 4px;">Need help referencing? Log in to <a href="https://shibboleth2sp.pm.semcs.net/Shibboleth.sso/Login?entityID=https%3A%2F%2Fidp.uwl.ac.uk%2Fidp%2Fshibboleth&target=https%3A%2F%2Fshibboleth2sp.pm.semcs.net%2Fshib%3Fdest%3Dhttp%253A%252F%252Fwww.citethemrightonline.com%252FShibValidate">Cite Right</a></div>');
                 
         }
     }

e842554b110bb3eb72a1e5289da4ba351988a9fc
mborg <mattborg1@gmail.com>
2018-06-21 18:03:42 +0100

    Update summon.js

--- summon.js
+++ summon.js
@@ -1,5 +1,4 @@
-
-// We can insert a message at the top of the Summon Search Results (true = on, false = off)
+// Put a message at the top of the Summon Search Results (true = on, false = off)
 var showAlert = true;
 
 angular.module('summonApp.directives').directive("resultsFeed",
@@ -9,7 +8,7 @@ function() {
             if (showAlert)
                 iElement
                     .find('ul.list-unstyled')
-                    .prepend('<div id="custommessage" style="color: #a94442;background-color: #f2dede;border-color: #ebccd1;padding: 15px;margin-bottom: 20px;border: 1px solid transparent;border-radius: 4px;">Hey. This can be whatever we want. And we can include a <a href="https://fse.summon.serialssolutions.com">link.</a></div>');
+                    .prepend('<div id="hvhlmessage" style="color: #a94442;background-color: #f2dede;border-color: #ebccd1;padding: 15px;margin-bottom: 20px;border: 1px solid transparent;border-radius: 4px;">You can enter text here.<a href="https://google.co.uk">And this can be a link.</a></div>');
                 
         }
     }

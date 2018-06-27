c6337e42c95fb3759f37e037c20c083d32a42706
mborg <mattborg1@gmail.com>
2018-06-27 13:52:17 +0100

    Update summon.js

--- summon.js
+++ summon.js
@@ -8,7 +8,7 @@ function() {
             if (showAlert)
                 iElement
                     .find('ul.list-unstyled')
-                    .prepend('<div id="hvhlmessage" style="color: #a94442;background-color: #f2dede;border-color: #ebccd1;padding: 15px;margin-bottom: 20px;border: 1px solid transparent;border-radius: 4px;">You can enter text here.<a href="https://google.co.uk">And this can be a link.</a></div>');
+                    .prepend('<div id="hvhlmessage" style="color: #a94442;background-color: #f2dede;border-color: #ebccd1;padding: 15px;margin-bottom: 20px;border: 1px solid transparent;border-radius: 4px;">You can enter text here.<a href="https://google.co.uk"> And this can be a link.</a></div>');
                 
         }
     }
